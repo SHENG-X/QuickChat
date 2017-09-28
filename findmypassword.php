d7f229163b72beae4023ed199bf0700fde46eceb
sheng <you@example.com>
2017-09-28 10:44:31 -0700

    generated password change code and with submiting timestamp

--- findmypassword.php
+++ findmypassword.php
@@ -3,9 +3,10 @@ include('classes/DB.php');
 if(isset($_POST['email'])&&$_POST['email']!=''&&filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
 	if(DB::query("SELECT email FROM user WHERE email=:email",array(":email"=>$_POST['email']))){
 		$userid=DB::query("SELECT id FROM user WHERE email=:email",array(":email"=>$_POST['email']))[0]['id'];
+		$cstrong=true;
+		$token=openssl_random_pseudo_bytes(64,$cstrong);
+		DB::query('INSERT INTO vcode(code,userid) VALUES(:code,:userid)',array(":code"=>sha1($token),":userid"=>$userid));
 		echo $userid;
-
-
 	}
 	else{
 		echo "Email does not exists!";

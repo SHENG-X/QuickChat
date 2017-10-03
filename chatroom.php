da9230f9404744a845893d602abcbc2ec3898f64
sheng <you@example.com>
2017-10-02 22:34:26 -0700

    new layout

--- chatroom.php
+++ chatroom.php
@@ -46,15 +46,23 @@
       padding-left: 10px;
       padding-right: 10px;
     }
+    #testing p{
+      word-wrap: break-word;
+    }
+    textarea{
+      border-left: none;
+      border-right: 1px solid #aaa;
+      border-top: 1px solid #aaa;
+    }
   </style>
 
   </head>
 <body>
 
   <div class='col-sm-3' style='height: 100vh;padding: 0px'>
-    <div style='height:8vh;background: white'>
-      <div style='height:8vh;background: white;' class='col-sm-2' id='settingcontainer'>
-        <i class="fa fa-cog" style="font-size:46px;position: relative;top:calc(4vh - 23px);left:calc(50% - 20px);color: #777" id='setting' class='menu'></i>
+    <div style='height:8vh;'>
+      <div style='height:8vh;border-right: 1px solid #aaa' class='col-sm-2' id='settingcontainer'>
+        <i class="fa fa-cog" style="font-size:46px;position: relative;top:calc(4vh - 23px);left:calc(50% - 20px);color: #777;" id='setting' class='menu'></i>
         <div id='settingitem' class="col-sm-10" style="background: #ffffff;z-index: 99;position: absolute;left:100%;top: 0px;max-height: 100px;padding: 0px;">
            <div class="btn-group-vertical">
               <button type="button" class="btn btn-default" data-toggle="modal" data-target="#myModal" id='changeprofile'>Change My Profile Image</button>
@@ -63,7 +71,7 @@
            </div>
         </div>
       </div>
-      <div style='height:8vh;background: red;padding: 0px' class='col-sm-10'>
+      <div style='height:8vh;padding: 0px;border-right: 1px solid #aaa' class='col-sm-10'>
             <form action="" class="search-form col-sm-12">
                 <div class="form-group has-feedback">
                   <label for="search" class="sr-only">Search</label>
@@ -74,16 +82,18 @@
       </div>
     </div>
     <div>
-      <div style='height:92vh;background: red;' class='col-sm-2'>
+      <div style='height:92vh;border-right: 1px solid #aaa' class='col-sm-2'>
         <hr>
         <i class="fa fa-sign-out" style="font-size:36px;position:relative;left: calc(50% - 15px);color: #777" title="Sign Out" id='signout'></i>
         <hr>
         <i class="fa fa-sitemap" style="font-size:36px;position:relative;left: calc(50% - 18px);color: #777" title="Create Group" id='creategroup' data-toggle="modal" data-target="#myModal"></i>
         <hr>
         <i class="fa fa-plus-circle" style="font-size:36px;position:relative;left: calc(50% - 18px);color: #777" title='Add Group' id='addgroup' data-toggle="modal" data-target="#myModal"></i>
+         <hr>
+        <i class="fa fa-search" style="font-size:36px;position:relative;left: calc(50% - 18px);color: #777" title='Look up user info' id='lookup' data-toggle="modal" data-target="#myModal2"></i>
       </div>
-      <div style='height:92vh;background: blue;padding: 0px' class='col-sm-10'>
-        <ul class="list-group col-sm-12" id="groups-list">
+      <div style='height:92vh;padding: 0px;border-right: 1px solid #aaa' class='col-sm-10'>
+        <ul class="list-group col-sm-12" id="groups-list" style="background:white;">
           <!--Where group names goes-->
         <!--  <li class="list-group-item">
               <div class="col-xs-12 col-sm-4">
@@ -101,39 +111,15 @@
     </div>
   </div>
 
-  <div class='col-sm-7' style='background: green;height: 100vh;padding: 0px'>
+  <div class='col-sm-9' style='height: 100vh;padding: 0px;'>
+    <div id ='cover' style="width: 100%;height: 100vh;position:fixed;background: white;z-index: 99;background-image: url('http://www.powerpointhintergrund.com/uploads/2017/06/simple-backgrounds-pictures-wallpaper-cave-1.jpeg');background-size: cover;"></div>
 
-     <div style='height:8vh;background: white;text-align: center;display: table-cell;vertical-align: middle;width:100vw'><h4 ><span id='messagetitle'>Quick Chat</span></h4></div>
-     <div id='testing' style="background:white;height: 80vh;overflow-y: auto;">
+     <div style='height:8vh;background: white;text-align: center;display: table-cell;vertical-align: middle;' class="col-sm-12"><h4 ><span id='messagetitle'>Quick Chat</span></h4></div>
+    
+     <div id='testing' style="height: 80vh;overflow-y: auto;overflow-x: hidden; padding: 0px" class="col-sm-12" >
       <!--Where message goes-->
-
-
-    <div class="media" style="width: 60%">
-      <div class="media-left">
-        <img src="https://www.w3schools.com/w3css/img_avatar3.png" class="media-object" style="width:45px">
-      </div>
-      <div class="media-body">
-        <h4 class="media-heading">John Doe <small><i>Posted on February 19, 2016</i></small></h4>
-        <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      </div>
-    </div>
-
-
-    <div class="media" style="width: 60%;position: relative;left:40%;">
-      <div class="media-body">
-        <h4 class="media-heading" >John Doe <small><i>Posted on February 19, 2016</i></small></h4>
-        <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      </div>
-      <div class="media-right">
-        <img src="https://www.w3schools.com/w3css/img_avatar3.png" class="media-object" style="width:45px">
-      </div>
-    </div>
-
-
-
-
-
      </div>
+
       <div>
          <!--Message control-->
          <form style='height:12vh;background: red;position:absolute;bottom: 0px;z-index: 10;width: 100%'>
@@ -147,11 +133,12 @@
           </div>
         </form>
       </div>
+            <div style="display: none;" id='testingmessage'></div>
     </div>
 
 
 
-  <div class='col-sm-2' style='background: blue;height: 100vh;padding: 0px'>
+ <!-- <div class='col-sm-2' style='height: 100vh;padding: 0px'>
     <div style='height:8vh;background: yellow;text-align: center;'>
         <i class="fa fa-user-circle-o" style="font-size:2.5em;position: relative;top: 1vh"></i>
     </div>
@@ -170,9 +157,8 @@
       </table>
     </div>
     </div>
-  </div>
+  </div>-->
 
-  <div style="display: none;" id='testingmessage'></div>
 
   <!-- Modal -->
   <div class="modal fade" id="myModal" role="dialog">
@@ -214,6 +200,38 @@
 
             <br/>
           </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+  <!--Look up user modal window-->
+  <div class="modal fade" id="myModal2" role="dialog">
+    <div class="modal-dialog modal-sm">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">My Info</h4>
+        </div>
+        <div class="modal-body" style="text-align: center;">
+          <div id='profile_info'>
+            <table style="width: 100%">
+              <tr><td>Name:</td><td>Miky Don</td></tr>
+              <tr><td>Date of Birth:</td><td>Jul. 12,1988</td></tr>
+              <tr><td>Gender:</td><td>Female</td></tr>
+              <tr><td>Country:</td><td>Canada</td></tr>
+              <tr><td>Language:</td><td>English</td></tr>
+            </table>
+          </div>
+          <hr>
+          Enter Username:
+          <input type="text" id='user-name' placeholder="Username"><input type="button" id='lookupname' value="Lookup">
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
         </div>
       </div>
     </div>
@@ -294,6 +312,7 @@
 
 $('document').ready(function(){
   $('li').click(function(){
+    $("#cover").hide();
     var targetvar='#'+this.id+' h4';
     $("#messagetitle").text($(targetvar).text());
     var group=$("#messagetitle").text();
@@ -344,7 +363,36 @@ $('#sendmessage').click(function(){
     });
   }
 });
+$('#message').keydown(function (e){
+    if(e.keyCode == 13){
+      var message=$('#message').val();
+  var group=$("#messagetitle").text();
+  message=replaceAll(message,'<','&lt');
+  message=replaceAll(message,'>','&gt');
+  if(message==''){
+    alert('Can not send empty message!');
+  }
+  else{
+    $.ajax({
+      url:'messagecontrol.php',
+      data:{'group':group,'message':message},
+      dataType:'text',
+      success:function(data){
+        $('#testing').html(data);
+        var objDiv = document.getElementById("testing");
+        objDiv.scrollTop = objDiv.scrollHeight;
+        $('#message').val('');
+      },
+      type:'POST'
+    });
+  }
+    }
+})
 
+
+
+
+//message feedback display control
 $('document').ready(function(){
   setInterval(function(){
     if($('#messagetitle').html()!='Quick Chat'){
--- messagecontrol.php
+++ messagecontrol.php
@@ -11,10 +11,10 @@ DB::query('INSERT INTO chatroom(groupid,userid,message) VALUES(:groupid,:userid,
 $allmessage=DB::query('SELECT * FROM chatroom,user WHERE groupid=:groupid AND chatroom.userid=user.id',array(':groupid'=>$groupid));
 foreach($allmessage as $i){
 	if($i['userid']==$userid){
-	echo "<div class=\"media\" style=\"width: 60%;position: relative;left:40%;\"> <div class=\"media-body\"><h4 class=\"media-heading\">".$i['username']."<small><i style='font-size:9px'> (".$i['time'].")</i></small></h4><hr style='margin:0px;'><p>".$i['message']."</p></div><div class=\"media-left\"><img class='img-circle' src=\"https://www.w3schools.com/w3css/img_avatar3.png\" class=\"media-object\" style=\"width:45px\"></div></div>";
+	echo "<div class=\"media\" style=\"width:100%;\"> <div class=\"media-body\"><h4 class=\"media-heading\" style='text-align:right'>".$i['username']."<small><i style='font-size:9px'> (".$i['time'].")</i></small></h4><hr style='margin:0px;'><div style='background:blue;'><p style='width: 29vw;float:right;text-align:right'>".$i['message']."</p></div></div><div class=\"media-right\"><img class='img-circle' src=\"https://www.w3schools.com/w3css/img_avatar3.png\" class=\"media-object\" style=\"width:45px\"></div></div>";
 	}
 	else{
-		echo "<div class=\"media\" style=\"width: 60%\"> <div class=\"media-left\"><img class='img-circle' src=\"https://www.w3schools.com/w3css/img_avatar3.png\" class=\"media-object\" style=\"width:45px\"></div> <div class=\"media-body\"><h4 class=\"media-heading\">".$i['username']."<small><i style='font-size:9px'> (".$i['time'].")</i></small></h4><hr style='margin:0px;'><p>".$i['message']."</p></div></div>";
+		echo "<div class=\"media\" style=\"width:100%;\"> <div class=\"media-left\"><img class='img-circle' src=\"https://www.w3schools.com/w3css/img_avatar3.png\" class=\"media-object\" style=\"width:45px\"></div><div class=\"media-body\"><h4 class=\"media-heading\">".$i['username']."<small><i style='font-size:9px'> (".$i['time'].")</i></small></h4><hr style='margin:0px;'><p style='width: 30vw;text-align:left'>".$i['message']."</p></div></div>";
 	}
 }
 

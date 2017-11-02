5e8d9725a77f1b19fa0af244bd43e145b7d46e84
SHENG <sheng26v@gmail.com>
2017-11-01 21:55:04 -0700

    Imporved mobile UI and identified SSE to reduce traffic

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <?php
+<!--   <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+ -->  <?php
     include ('classes/Login.php');
     if(!Login::isLoggedin()){
       header('Location:login.html');
